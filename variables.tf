1aa19646e67d5a94dbfe47d9b33f36e6a64b9a84
Emmanuel Adediran <153106409+Xpriest-hud@users.noreply.github.com>
2025-08-22 21:10:24 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,5 @@
 variable "netlify_api_token" {
-  description = "Netlify API Token"
+  description = "Netlify Personal Access Token"
   type        = string
   sensitive   = true
-}
-
-variable "site_name" {
-  description = "Name of the Netlify site"
-  type        = string
-  default     = "terraform-demo-site"
 }
