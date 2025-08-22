1bf6aa4e1f48e8c7e789cf0a230010b41b5a4ace
Emmanuel Adediran <153106409+Xpriest-hud@users.noreply.github.com>
2025-08-22 21:10:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,36 @@
+terraform {
+  cloud {
+    organization = "HUG-NETLIFY-SITE"     # Replace with your HCP Terraform organization
+    workspaces {
+      name = "netlify-website"     # Replace with your HCP Terraform workspace
+    }
+  }
+
+  required_providers {
+    netlify = {
+      source  = "netlify/netlify"
+      version = "~> 0.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+# Configure Netlify provider
 provider "netlify" {
   token = var.netlify_api_token
 }
 
-# Random suffix so site name is unique
-resource "random_pet" "suffix" {}
-
-# Create Netlify site
-resource "netlify_site" "site" {
-  name = "${var.site_name}-${random_pet.suffix.id}"
+# Random suffix to ensure unique site name
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
 }
 
-# Deploy contents of site/ folder
-resource "netlify_deploy" "deploy" {
-  site_id = netlify_site.site.id
-  dir     = "${path.module}/site"
+# Create Netlify site
+resource "netlify_site" "this" {
+  name = "my-terraform-site-${random_string.suffix.result}"
 }
